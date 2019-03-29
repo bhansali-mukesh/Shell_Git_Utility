9e109576371dceea4ef9176f28845b76312217fc
Mukesh Kumar Bhansali <bhansali.mukesh@gmail.com>
2019-03-29 16:44:05 +0530

    Arranged Sections

--- Git.sh
+++ Git.sh
@@ -45,7 +45,7 @@ export master="main"
 # Going back to the calling directory when things are done
 alias Back='cd - > /dev/null; echo -e "\a" ';
 
-# Helpful Short-cut not related to Git
+		# Helpful Short-cut not related to Git
 
 # Clear Screen
 alias c=clear
@@ -56,15 +56,21 @@ alias a=alias
 # Goes to WorkSpace Directory
 alias Workspace="cd $WORKSPACE"
 
-# Utility Function to Support Other Aliases
+		# Utility Function to Support Other Aliases
+	
+
 # Validate Number of Input Provided
 alias Validator='function Validator() { actual_parameters=`expr $# - 2`; if [ $actual_parameters -lt $1 ]; then echo -e "\t $2" >&2; echo return; fi }; Validator'
 
-# Utility Function to Support Other Aliases
 # Confirms Whether User Wants to Continue
 alias Confirmer='function Confirmer() { echo -e "\n\t Press \"Y\" to Continue\n" >&2; read confirm; if [ "$confirm"T != "YT" ]; then echo -e "\t Action Cancelled\n" >&2; echo return; fi }; Confirmer'
 
 ###### ------------------------------------------ HELP SECTION ------------------------------------------ ######
+
+# Set Alias "Gitter" to Open this File
+# No Parameter
+alias Gitter="vi $FILENAME";
+
 
 # Just type "Help" on Terminal
 # If Help is Followed By Some Special Command then it will Show Help for that Command
@@ -75,31 +81,30 @@ alias Confirmer='function Confirmer() { echo -e "\n\t Press \"Y\" to Continue\n"
 		# 2. Help Commit
 alias Help='function JASOL() { Document=`Help_Document`; if [ M"$1" = "M" ]; then echo "$Document"| less; return; fi;  echo ""; echo "$Document" | sed -n "/^"$1"/,/^$/p"; }; JASOL'
 
+
 # Utility Function which Parses this Git File and Generates The Help Documents to Help, The Command "Help"
 # Generating Help Document from the Source on Runtime
 alias Help_Document='function MB() { Comment_Character="#"; Alias_Character="alias"; Help=""; Alias=""; while read line; do if [[ $line == $Comment_Character* ]]; then Help="$Help""\n""$line"; else Alias_Found=`echo "$line"| grep $Alias_Character`; if [[ M"$Alias_Found" != "M" ]]; then Alias=`echo "$line"| cut -d"=" -f1 | tr -s " "| cut -d" " -f2`; if [[ M"$Alias" != "M" ]]; then echo -e "\n$Alias" : "$Help"; Help=""; fi; else Help=""; fi; fi; done < "$FILENAME"; }; MB'
 
 
 
-###### ***************************************** GIT ALIASES ***************************************** ###### 
+###### ***************************************** GIT ALIASES ***************************************** ######
 
-# Set Alias "Gitter" to Open this File
+								# Repository and Branches
+
+# Set up Repositories
 # No Parameter
-alias Gitter="vi $FILENAME";
+alias CLONE='function KUMAR() { Workspace; cd ..; remotes=`git remote show`; for remote in $remotes; do echo "Cloning $remote ..."; remote_url=`git remote show $remote | grep -im1 URL | rev | cut -d" " -f1| rev`; git clone $remote_url; done; }; KUMAR'
 
-# Open Git GUI
-# No Parameter
-alias Git="git gui"
 
-# Publish Git Commits to Gerrit
-# Optional Parameter
-alias Publish='function BHAWANA() { echo -e "\n\t Do You Want Publish Now ( Recommended to \"Combine ( All Commits )\" First, Ignore if Already Combined ) ?\n"; `Confirmer`; git publish; }; BHAWANA'
-
-# Show Difference for Particular File
-# Need Parameter
-# Example
-	# Diff README.html
-alias Diff="git diff"
+# Creates and checks out the same git branch
+# You can switch to your branch any time by just typing it's Jira id
+# Example 
+#        Branch Name : NGCONT-1745_Infiniti-Create-new-homepage-outlines
+#        Just Fire Command : Create NGCONT-1745_Infiniti-Create-new-homepage-outlines
+# Now if I type "1745" on terminal Anytime, I am switched to this Branch
+# Need Parameter      
+alias Create='function MBMB(){ Workspace; `Validator 1 "Need a Parameters ( Branch Name )" $@`; story=`echo $1| cut -d'_' -f1`; echo "alias $story=\"From=`Current`; git checkout $1 \"" >> $FILENAME; git checkout -b $1 $origin/$master; echo -e "\t\tType " \"$story\" " to Jump to this Branch, Anytime"; source $FILENAME; };MBMB'
 
 # Shows All Branches in Repository
 # No Parameter
@@ -107,6 +112,10 @@ alias Bbranch="git branch";
 
 # Another Short Hand For briefing git Branches
 alias B=Bbranch;
+
+# Briefs for all current local git branches
+# No Parameter
+alias Branch='git branch -vv';
 
 # Rename a Branch
 # Need 2 Parameters (1. SourceName  2. NewName )
@@ -122,6 +131,10 @@ alias All='git branch -a'
 # No Parameter
 alias Current='Branch| grep \* | cut -d" " -f2;'
 
+# Goes to PREVIOUS git branch
+# No Parameter
+alias Previous='Present=$From; Checkout $Present';
+
 # Checkout to a git branch
 # Also Useful For Reverting Local Changes for a Particular File
 # Need Parameter
@@ -129,19 +142,6 @@ alias Current='Branch| grep \* | cut -d" " -f2;'
 	# 1.	Checkout Master
 	# 2.	Checkout REAME.html
 alias Checkout='From=`Current`; git checkout'
-
-# Creates and checks out the same git branch
-# You can switch to your branch any time by just typing it's Jira id
-# Example 
-#        Branch Name : NGCONT-1745_Infiniti-Create-new-homepage-outlines
-#        Just Fire Command : Create NGCONT-1745_Infiniti-Create-new-homepage-outlines
-# Now if I type "1745" on terminal Anytime, I am switched to this Branch
-# Need Parameter      
-alias Create='function MBMB(){ Workspace; `Validator 1 "Need a Parameters ( Branch Name )" $@`; story=`echo $1| cut -d'_' -f1`; echo "alias $story=\"From=`Current`; git checkout $1 \"" >> $FILENAME; git checkout -b $1 $origin/$master; echo -e "\t\tType " \"$story\" " to Jump to this Branch, Anytime"; source $FILENAME; };MBMB'
-
-# Goes to PREVIOUS git branch
-# No Parameter
-alias Previous='Present=$From; Checkout $Present';
 
 # Goes to Master/Main Branch
 # No Parameter
@@ -151,109 +151,36 @@ alias Master='Checkout $master';
 # No Parameter
 alias Origin='git pull $origin $master';
 
-# Remove git branch
-# Need Parameter
-# Example
-	# Remove OLD_BRANCH
-alias Remove="git branch -D";
 
-# Deletes the branch you are currently on
-# Need Confirmation
-alias Delete='Me=`Current`; echo -e "\n\t Do You Want to Delete Branch \"$Me\" \n"; `Confirmer`; Master; Remove $Me;'
 
-# Adds files to Commit
-# Need Parameter(s)
-# Example
-	# Add README.html
-alias Add='git add';
-
-# Commits Added File in Branch for Permanent Changes
-# Need Message Parameter
-# Example
-	# Commit "Changes to Improve Performance"
-alias Commit='git commit -m';
-
-# Amends Last Commit in Branch for Permanent Changes
-# Need Message Parameter
-# Example
-	# Aamend "Amending Changes to Improve Performance"
-alias Aamend='git commit --amend -m'
-
-# Combine Multiple Commits
-# Need 2 Parameters ( 1. Number of Commits, 2. Commit Message )
-# Example
-	# Combine 4 "Combining 4 Commits into one Single Commit"
-alias Combine='function JODHPUR() { `Validator 2 "Need 2 Parameters ( 1. Number of Commits, 2. Commit Message )" $@`; git reset --soft HEAD~$1; git commit -m "$2"; }; JODHPUR'
-
-# Remove Commits From History, However Changes Stays in Work Directory
-# Need a Parameters ( Number of Commits )
-# Example
-	# UnCommit 3
-alias UnCommit='function ADI() { `Validator 1 "Need 1 Parameters ( Number of Commits )" $@`; git reset --soft HEAD~$1; }; ADI'
-
-# Push Current Branch
+								# Changes Workflow
+								
+										## Review Changes
+										
+# Gives List of All Local Changes ( Whether it is not in Remote Repository )
 # No Parameter
-alias Push='me=`Current`; git push --set-upstream origin $me'
-
-# It Needs "Commit Message" in double quotes as Parameter.
-# Add, Commit and Push in 1 Shot ( Makes Our Branch Available in Remote Server, Also Used in Recovery, Share and Review Changes )
-# Need Confirmation and Commit Message
-# Example
-	# Stage "Issue Resolved"
-alias Stage='function MKBJ(){ `Validator 1 "Need a Parameters ( Commit Message )" $@`; changes=`M|wc -l`; if [ "$changes" -eq 0 ]; then echo -e "\n\tNo Changes to Stage\n"; return; fi; echo -e "\n\t$changes file(s) are identified to Stage\n"; M; `Confirmer`; Add .; Commit "$1";}; MKBJ'
-
-
-# It Needs "Commit Message" in double quotes as Parameter.
-# Add, Commit and Push in 1 Shot ( Makes Our Branch Available in Remote Server, Also Used in Recovery, Share and Review Changes )
-# Need Confirmation and Commit Message
-# Example
-	# Amend "Refactoring Done"
-alias Amend='function VIMLESH(){ `Validator 1 "Need a Parameters ( Commit Message )" $@`; changes=`M|wc -l`; if [ "$changes" -eq 0 ]; then echo -e "\n\tNo Changes to Stage\n"; return; fi; echo -e "\n\t$changes file(s) are identified to Stage\n"; M; `Confirmer`; Add .; Aamend "$1";}; VIMLESH'
-
-
-# Briefs for all current local git branches
-# No Parameter
-alias Branch='git branch -vv';
-
-# Fetches Remote Branch into your Local But DO NOT Merge Changes into Your Local Branch.
-# No Parameter
-alias Ffetch='git fetch';
-
-# Before fetching, remove any remote-tracking references that no longer exist on the remote ( Recommended )
-# Need Parameter
-alias Fetch='git fetch --prune';
-
-# Merges Changes into Branch
-# Need Parameter
-alias Merge='git merge';
-
-# Fetches Remote Branch into your Local and Merges Changes into Your Local Branch ( Fetch + Merge ) .
-# No Parameter
-alias Ppull='git pull';
-
-# Goes to Workspace, Pulls from Remote, Comes Back 
-# No Parameter
-alias Pull='Workspace; No_Changes=`Save|grep -i "No local changes"| wc -l`; Ppull; if [ $No_Changes -eq 0 ]; then Pop; fi; Back';
+alias ?="git status"
 
 # Gives List of All Modified Files only ( File Should be in Remote Repository )
 # No Parameter
 alias M='function Changes() { git status | grep -E "deleted|modified|new file"; git ls-files . --exclude-standard --others; }; Changes';
 
-# Gives List of All Local Changes ( Whether it is not in Remote Repository )
-# No Parameter
-alias ?="git status"
+# Show Difference for Particular File
+# Need Parameter
+# Example
+	# Diff README.html
+alias Diff="git diff"
 
-# Delete All Local git branches except Master
-# No Parameter
-alias Delete_But='Master; git branch | tr -s " "| cut -d" " -f2 | grep -v $master | xargs git branch -d;'
 
-# List all the Stashes
-# No Parameter
-alias List="git stash list"
-
+										## Temporary Changes ( Not in any Branch )
+										
 # Saves Change Point to Rollback in Future
 # No Parameter
 alias Sstash='git stash';
+
+# Saves Change Point to Rollback in Future
+# No Parameter
+alias Stash='git stash -u';
 
 # Saves Change Point with Name to Rollback in Future, If Needed ( Needs a Parameter, Checkpoint Name )
 # No Parameter
@@ -264,6 +191,10 @@ alias Ssave='BranchName=`Current`; git stash save $BranchName'
 # Undo local changes
 # No Parameter
 alias Save='BranchName=`Current`; git stash save -u $BranchName';
+
+# List all the Stashes
+# No Parameter
+alias List="git stash list"
 
 # Goes to Latest Saved Change Point
 # No Parameter
@@ -281,6 +212,9 @@ alias Apply='function BHANSALI() { git stash apply stash@{$1}; }; BHANSALI'
 	# Drop 0
 alias Drop='function BHANSALI() { git stash drop stash@{$1}; }; BHANSALI'
 
+
+										## Revert Changes
+										
 # Clean UnTracked Files and Directories
 # No Parameter
 alias Clean='git clean -fdx';
@@ -306,7 +240,7 @@ alias RESET='RRESET; Checkout .'
 # DANGEROUS COMMAND : You May Lose Your Changes
 # Revert Code to Origin Master/Main
 # No Parameter
-alias HARD_RESET='Fetch; git reset --hard $origin/$master; Clean'
+alias HARD_RESET='Fetch; git reset --hard $origin/$master; Clean; Pull'
 
 		# Rebase to Origin Master Branch
 		# No Parameter
@@ -315,26 +249,121 @@ alias HARD_RESET='Fetch; git reset --hard $origin/$master; Clean'
 # Abort Rebase Operation
 # No Parameter
 alias Abort_Rebase='git rebase --abort'
-		
-# Rewrite Commit History, To Undo Commit, Edit Message and More
-# Need Parameter ( Go How Many Commits Behind, To Rewrite History )
-# Example
-	# Rewrite 5
-alias Rewrite='function OK() { `Validator 1 "Need a Parameters ( Number of Commits )" $@`; head=""; commits=$1; commit=0; while [ $commit -lt $commits ]; do head="$head"^; commit=`expr $commit + 1`; done; git rebase -i HEAD$head ; }; OK'
 
-# DANGEROUS COMMAND : One Should Not Use it on Public/Remote Repositories
-# Rollback Commits, You May Lose Changes done in Last Specified Number of Commits which you Specifies
-# It is What is Expected from this Command to Rollback Some Commits.
-# You Need to Specify, How Many Commits Needs to be Rolled Back as Parameter
-# Need Parameter ( Go How Many Commits Needs to be Rolled Back )
-# Example
-	# ROLLBACK 2
-alias ROLLBACK='function OK() { `Validator 1 "Need a Parameters ( Number of Commits )" $@`; head=""; commits=$1; commit=0; while [ $commit -lt $commits ]; do head="$head"^; commit=`expr $commit + 1`; done; git reset --hard HEAD$head ; }; OK'
 
+										## Get Changes From Remote Repository
+										
 # Link Missing Jars
 # No Parameter
 alias Link='git phat init; git phat link'
 
+# Fetches Remote Branch into your Local But DO NOT Merge Changes into Your Local Branch.
+# No Parameter
+alias Ffetch='git fetch';
+
+# Before fetching, remove any remote-tracking references that no longer exist on the remote ( Recommended )
+# Need Parameter
+alias Fetch='git fetch --prune';
+
+# Merges Changes into Branch
+# Need Parameter
+alias Merge='git merge';
+
+# Fetches Remote Branch into your Local and Merges Changes into Your Local Branch ( Fetch + Merge ) .
+# No Parameter
+alias Ppull='git pull';
+
+# Goes to Workspace, Pulls from Remote, Comes Back 
+# No Parameter
+alias Pull='Workspace; No_Changes=`Save|grep -i "No local changes"| wc -l`; Ppull; if [ $No_Changes -eq 0 ]; then Pop; fi; Back';
+
+
+											## Add Local Changes ( To Local Branch, Nothing to Remote Yet )
+											
+# Opens Git GUI
+# No Parameter
+alias Git="git gui"
+# Adds files to Commit
+# Need Parameter(s)
+# Example
+	# Add README.html
+alias Add='git add';
+
+# Un Adds files From Staging
+# Need Parameter(s)
+# Example
+	# UnAdd README.html
+alias UnAdd='Rreset';
+
+# Commits Added File in Branch for Permanent Changes
+# Need Message Parameter
+# Example
+	# Ccommit "Changes to Improve Performance"
+alias Ccommit='git commit -m';
+
+# It Needs "Commit Message" in double quotes as Parameter.
+# Add, Commit and Push in 1 Shot ( Makes Our Branch Available in Remote Server, Also Used in Recovery, Share and Review Changes )
+# Need Confirmation and Commit Message
+# Example
+	# Commit "Issue Resolved"
+alias Commit='function MKBJ(){ `Validator 1 "Need a Parameters ( Commit Message )" $@`; changes=`M|wc -l`; if [ "$changes" -eq 0 ]; then echo -e "\n\tNo Changes to Stage\n"; return; fi; echo -e "\n\t$changes file(s) are identified to Stage\n"; M; `Confirmer`; Add .; Ccommit "$1";}; MKBJ'
+
+# Amends Last Commit in Branch for Permanent Changes
+# Need Message Parameter
+# Example
+	# Aamend "Amending Changes to Improve Performance"
+alias Aamend='git commit --amend -m'
+
+# It Needs "Commit Message" in double quotes as Parameter.
+# Add, Commit and Push in 1 Shot ( Makes Our Branch Available in Remote Server, Also Used in Recovery, Share and Review Changes )
+# Need Confirmation and Commit Message
+# Example
+	# Amend "Refactoring Done"
+alias Amend='function VIMLESH(){ `Validator 1 "Need a Parameters ( Commit Message )" $@`; changes=`M|wc -l`; if [ "$changes" -eq 0 ]; then echo -e "\n\tNo Changes to Stage\n"; return; fi; echo -e "\n\t$changes file(s) are identified to Stage\n"; M; `Confirmer`; Add .; Aamend "$1";}; VIMLESH'
+
+# Remove Commits From History, However Changes Stays in Work Directory
+# Need a Parameters ( Number of Commits )
+# Example
+	# UnCommit 3
+alias UnCommit='function ADI() { `Validator 1 "Need 1 Parameters ( Number of Commits )" $@`; git reset --soft HEAD~$1; }; ADI'
+
+# Combine Multiple Commits
+# Need 2 Parameters ( 1. Number of Commits, 2. Commit Message )
+# Example
+	# Combine 4 "Combining 4 Commits into one Single Commit"
+alias Combine='function JODHPUR() { `Validator 2 "Need 2 Parameters ( 1. Number of Commits, 2. Commit Message )" $@`; git reset --soft HEAD~$1; git commit -m "$2"; }; JODHPUR'
+
+
+											## Push Changes to Remote
+											
+# Publish Git Commits to Remote
+# Optional Parameter
+alias Publish='function BHAWANA() { echo -e "\n\t Do You Want Publish Now ( Recommended to \"Combine ( All Commits )\" First, Ignore if Already Combined ) ?\n"; `Confirmer`; git publish; }; BHAWANA'
+
+# Push Current Branch to Remote
+# No Parameter
+alias Push='me=`Current`; git push --set-upstream origin $me'
+
+
+											## Delete Un Used Branches
+										
+# Remove git branch
+# Need Parameter
+# Example
+	# Remove OLD_BRANCH
+alias Remove="git branch -D";
+
+# Deletes the branch you are currently on
+# Need Confirmation
+alias Delete='Me=`Current`; echo -e "\n\t Do You Want to Delete Branch \"$Me\" \n"; `Confirmer`; Master; Remove $Me;'
+
+# Delete All Local git branches except Master
+# No Parameter
+alias Delete_But='Master; git branch | tr -s " "| cut -d" " -f2 | grep -v $master | xargs git branch -d;'
+
+								
+										# Log Views
+										
 # Git Log
 # No Parameter
 alias Log='git log';
@@ -361,20 +390,41 @@ alias Today='git log --after="yesterday"';
 # No Parameter
 alias Yesterday='git log --since=yesterday.midnight';
 
-# Set up product-root environment
-#alias SETUP_ROOT='git clone http://'"$USER"'@stash.cdk.com/scm/bhansali/product-root.git; npm run setup';
 
-# Set up Repositories
-# No Parameter
-alias CLONE='function KUMAR() { Workspace; cd ..; remotes=`git remote show`; for remote in $remotes; do echo "Cloning $remote ..."; remote_url=`git remote show $remote | grep -im1 URL | rev | cut -d" " -f1| rev`; git clone $remote_url; done; }; KUMAR'
-	
+
+										# ADVANCE Section
+
+# Reverts Given (Id) Commit
+# Needs a Parameter ( Commit Id ) to Revert 
+alias REVERT='git revert';
+
 # Creates a New Commit which will nullify the effect of Last Commit
 # No Parameter
 alias Revert_Last_Commit='Last_Commit=`git log | head -n 1 | cut -d" " -f2`; git revert $Last_Commit';
+
+# DANGEROUS COMMAND : One Should Not Use it on Public/Remote Repositories
+# Rollback Commits, You May Lose Changes done in Last Specified Number of Commits which you Specifies
+# It is What is Expected from this Command to Rollback Some Commits.
+# You Need to Specify, How Many Commits Needs to be Rolled Back as Parameter
+# Need Parameter ( Go How Many Commits Needs to be Rolled Back )
+# Example
+	# ROLLBACK 2
+alias ROLLBACK='function OK() { `Validator 1 "Need a Parameters ( Number of Commits )" $@`; head=""; commits=$1; commit=0; while [ $commit -lt $commits ]; do head="$head"^; commit=`expr $commit + 1`; done; git reset --hard HEAD$head ; }; OK'
+
+
+# DANGEROUS COMMAND : One Should Not Use it Untill He/She is confident about Usage
+# Rewrite Commit History, To Undo Commit, Edit Message and More
+# Need Parameter ( Go How Many Commits Behind, To Rewrite History )
+# Example
+	# Rewrite 5
+alias REWRITE='function OK() { `Validator 1 "Need a Parameters ( Number of Commits )" $@`; head=""; commits=$1; commit=0; while [ $commit -lt $commits ]; do head="$head"^; commit=`expr $commit + 1`; done; git rebase -i HEAD$head ; }; OK'
+
 
 # In case if you change your password and git doesn't know it
 # Erase git cached Password ( Press Enter Twice )
 # No Parameter
 alias Git_Erase_Password='git credential-osxkeychain erase';
 
-# Your Local Git Branch's Aliases Goes Here
+		
+		
+		# Your Local Git Branch's Aliases Goes Here
